c0c7b6f2b26baa165802b23eece530615d72e7fe
Rohan <skrohan0420@gmail.com>
2025-03-23 16:46:44 +0530

    My First Commit, Hello World !

---
+++ module/module.js
@@ -0,0 +1,6 @@
+// module.js (ES Module Syntax)
+export const sum = (a, b) => a + b;
+export const sub = (a, b) => a - b;
+export const mul = (a, b) => a * b;
+export const div = (a, b) => a / b;
+
---
+++ module/script.js
@@ -0,0 +1,8 @@
+import { sum, sub, mul, div } from './module.js';
+
+
+
+console.log(sum(2, 6));
+console.log(sub(5, 2));
+console.log(mul(3, 4));
+console.log(div(66, 3));
